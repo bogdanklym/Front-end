b431967f8768079fe8942df164d7143981783a33
Bogdan Klym <bogdanklym2008@gmail.com>
2026-05-12 20:35:13 +0200

    Add files via upload
    no incluye node_modules (limite de 100 elementos) Esta hecho con React

---
+++ login-singup/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSingup from './Components/LoginSingUp/LoginSIngup';
+
+function App() {
+  return (
+    <div>
+      <LoginSingup/>
+    </div>
+  );
+}
+
+export default App;
